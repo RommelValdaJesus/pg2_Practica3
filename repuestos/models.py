87877204a03e5ad85e60578072f560ce5bc21d5c
RommelValdaJesus <rommelvalda.adm23ch@tecba.edu.bo>
2025-06-05 20:19:09 -0400

    README ACTUALIZADO

--- repuestos/models.py
+++ repuestos/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Proveedor(models.Model):
     nombre = models.CharField(max_length=100)
     contacto = models.CharField(max_length=100)
+    email = models.EmailField()
     ubicacion = models.CharField(max_length=255)
 
     def __str__(self):
@@ -17,7 +18,7 @@ class Marca(models.Model):
 
 class Vehiculo(models.Model):
     modelo = models.CharField(max_length=100)
-    año = models.IntegerField()
+    año = models.PositiveIntegerField()
     marca = models.ForeignKey(Marca, on_delete=models.CASCADE, related_name="vehiculos")
 
     def __str__(self):
@@ -27,8 +28,9 @@ class Repuesto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
+    moneda = models.CharField(max_length=3, choices=[("USD", "Dólares"), ("BOB", "Bolivianos")], default="BOB")
     proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE, related_name="repuestos")
-    vehiculo = models.ForeignKey(Vehiculo, on_delete=models.CASCADE, related_name="repuestos")
+    vehiculos = models.ManyToManyField(Vehiculo, related_name="repuestos")
 
     def __str__(self):
         return self.nombre
